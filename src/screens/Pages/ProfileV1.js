6317498982d7cd8242659ebef1e7c6405c745ed2
Samkelo <samkelo@techswat.co.za>
2022-01-14 11:35:16 +0200

    Re sized signature pad

--- src/screens/Pages/ProfileV1.js
+++ src/screens/Pages/ProfileV1.js
@@ -558,7 +558,7 @@ class ProfileV1Page extends React.Component {
               ? <>
                 <p>If you agree to the above document, please enter your signature:</p>
                 <div className="border border-primary border-2 p-3" style={{
-                  width: '40%'
+                  width: '100%'
                   }}>
                   <SignatureCanvas className="border border-primary border-2" penColor='black'
                   canvasProps={{  height: 200, className: 'sigCanvas' }} ref={(ref) => { this.sigPad = ref }} />
