168e27beb7ba72821b5a152f3b454d4de97d49cb
Samkelo Ntobeko  Zondi <samkeloz@techswat.co.za>
2022-09-01 08:35:29 +0200

    temp fix to upload

--- src/components/Pages/ProfileV1Setting.js
+++ src/components/Pages/ProfileV1Setting.js
@@ -800,6 +800,7 @@ this.props.updateStudentName(
                 className="user-photo media-object"
                 width="150px"
                 src={this.state.imageUrl} />
+                {myButton}
               {localStorage.getItem('role') == 'admin' ?null : myButton}
             </div>
             <div className="media-body">
