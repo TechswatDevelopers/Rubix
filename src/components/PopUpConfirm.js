afa5dd796dab96699e0dbbfcfe59c7a22879bab7
Samkelo Ntobeko  Zondi <samkeloz@techswat.co.za>
2023-03-06 13:19:34 +0200

    fixed

--- src/components/PopUpConfirm.js
+++ src/components/PopUpConfirm.js
@@ -363,6 +363,7 @@ getUserWitnessData() {
                 </>
                 : null
               }
+              
               <label className="control-label" >
                           Vet Comment
                             </label>
